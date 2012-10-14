f0a4c33dd12944fd35dbb69a73f9a2a4141be86e
Saulo Ricci <infsaulok@gmail.com>
2012-10-13 22:32:00 -0300

    level1

---
+++ README.txt
@@ -0,0 +1,8 @@
+Este é um repositório para o conjunto de arquivos que representam as
+soluções para os respectivos níveis do desafio da Sieve. 
+O objetivo é extrair os preços que estão escondidos em cada nível deste
+desafio.
+
+Por convenção adotou-se que cada arquivo terá a seguinte nomeação:
+levelx.py onde x representa o número do nível em questão.
+
---
+++ level1.py
@@ -0,0 +1,16 @@
+import urllib2
+
+
+def main():
+    url = 'http://hughes.sieve.com.br:9090/level1/'
+    
+    # Definindo User-Agent como Google Chrome
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.13 (KHTML, like Gecko) Chrome/24.0.1284.0 Safari/537.13'
+    header_dict = {'User-Agent': user_agent}
+    req = urllib2.Request(url, headers = header_dict)
+    response = urllib2.urlopen(req)
+    price_page = response.read()
+    print price_page
+
+if __name__ == '__main__':
+    main()
