b2d0662ce9f275cc89f558e5216cf532c2b6e77c
Saulo Ricci <infsaulok@gmail.com>
2012-10-14 23:46:12 -0300

    Níveis 2 e 3 do desafio adicionados.

--- level1.py
+++ level1.py
@@ -1,10 +1,11 @@
+#coding: utf-8
 import urllib2
 
 
 def main():
     url = 'http://hughes.sieve.com.br:9090/level1/'
     
-    # Definindo User-Agent como Google Chrome
+    # Definição do header User-Agent como Google Chrome
     user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.13 (KHTML, like Gecko) Chrome/24.0.1284.0 Safari/537.13'
     header_dict = {'User-Agent': user_agent}
     req = urllib2.Request(url, headers = header_dict)
---
+++ level3.py
@@ -0,0 +1,63 @@
+#coding: utf-8
+
+import os.path
+import urllib2
+
+def set_up_cookie_stuff():
+    COOKIEFILE = './cookies.txt'
+
+    cj = None
+    ClientCookie = None
+    cookielib = None
+    
+    try:
+        import cookielib
+    except ImportError:
+        try:
+            import ClientCookie
+        except ImportError:
+            urlopen = urllib2.urlopen
+            Request = urllib2.Request
+        else:
+            urlopen = ClientCookie.urlopen
+            Request = ClientCookie.Request
+            cj = ClientCookie.LWPCookieJar()
+    else:
+        urlopen = urllib2.urlopen
+        Request = urllib2.Request
+        cj = cookielib.LWPCookieJar()
+
+    if cj is not None:
+        if os.path.isfile(COOKIEFILE):
+            cj.load(COOKIEFILE)
+        if cookielib is not None:
+            opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+            urllib2.install_opener(opener)
+        else:
+            opener = ClientCookie.build_opener(ClientCookie.HTTPCookieProcessor(cj))
+            ClientCookie.install_opener(opener)
+
+def main():
+
+    set_up_cookie_stuff()
+
+    url = 'http://hughes.sieve.com.br:9090/level3/'
+    url2 = 'http://hughes.sieve.com.br:9090/level31/'
+    
+    # Definição de alguns parâmetros do Header
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.14 (KHTML, like Gecko) Version/6.0.1 Safari/536.26.14'
+    accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8Response HeadersNameValue'
+    accept_charset = 'utf-8'
+    header_dict = {'Accept': accept, 'Accept-Charset': accept_charset, 'User-Agent': user_agent}
+
+    req = urllib2.Request(url, headers= header_dict)
+    response = urllib2.urlopen(req)
+    req = urllib2.Request(url2, headers= header_dict)
+    response = urllib2.urlopen(req)
+    req = urllib2.Request(url, headers= header_dict)
+    response = urllib2.urlopen(req)
+    price_page = response.read()
+    print price_page
+    
+if __name__ == '__main__':
+    main()
