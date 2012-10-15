7ec43016f09a2eea75b66bb49e5acf04f2c79bf5
Saulo Ricci <infsaulok@gmail.com>
2012-10-15 19:11:06 -0300

    Extração do preço por regex no nível 4

--- level4.py
+++ level4.py
@@ -3,6 +3,10 @@
 import urllib2
 import Cookie
 
+import re
+
+PRICE_PATTERN = '.*(R\$\s([0-9]\s?)+,([0-9]\s?)+).*'
+
 def main():
 	url = 'http://hughes.sieve.com.br:9090/level4/'
 	user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.14 (KHTML, like Gecko) Version/6.0.1 Safari/536.26.14'
@@ -12,7 +16,7 @@ def main():
 	req.add_header('Cookie', 'cade-meu-cookie=\"esta aqui\"')
 	response = urllib2.urlopen(req)
 	page = response.read()
-	print page
+	print re.findall(PRICE_PATTERN, page.strip())[0][0]
 	
 if __name__ == '__main__':
     main()
